6766c27f8b0c2266475694561b8f389e431bf475
Diyorbek <diyorbekanorboyev001@gmail.com>
2024-12-11 22:34:48 +0500

    finish project

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,30 +1,38 @@
 import React from 'react'
 import initTranslations from '@/i18n'
 import TranslationProvider from '@/providers/translate.provider'
+// import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 const i18nNamespaces = ['translation']
 
 const Layout = async ({
-  params,
-  children
+  children,
+  params
 }: {
-  params: { locale: string }
   children: React.ReactNode
+  params: any
 }) => {
-  const { locale } = params  // No need to await params
-
   // Fetch translations based on the locale
+  const { locale } = await params
   const { resources } = await initTranslations(locale, i18nNamespaces)
 
   return (
-    <TranslationProvider
-      locale={locale}
-      resources={resources}
-      namespaces={i18nNamespaces}
-    >
-      {children}
-    </TranslationProvider>
+    <>
+      <TranslationProvider
+        locale={locale}
+        resources={resources}
+        namespaces={i18nNamespaces}
+      >
+        {children}
+      </TranslationProvider>
+    </>
   )
 }
 
 export default Layout
+
+// export const getServerSideProps = async ({ locales }: any) => ({
+//   props: {
+//     ...(await serverSideTranslations(locales, ['common']))
+//   }
+// })
